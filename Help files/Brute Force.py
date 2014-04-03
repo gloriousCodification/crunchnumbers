21a4565cb60d13c619a03ceb8a78de2e35d8f69d
JordRood <jordrood@gmail.com>
2014-04-03 10:48:46 +0200

    Toevoeging
    Help Files en Presentatie 1

---
+++ Help files/Brute Force.py
@@ -0,0 +1,45 @@
+## Brute Force Number Crunching
+
+import math
+
+def getS(i, startNumber):
+    """
+    Function returns the state space as a list after maximal i opreations
+    """
+
+    if i == 0:
+        s = []
+        return s
+
+    elif i == 1:
+        # Define states after one operation. Floor not included because
+        # this operation doesn't make sense. 
+        s = [("w", startNumber**.5), ("!", math.factorial(startNumber))]
+        return s
+
+    else:
+        prevS = getS(i-1, startNumber)
+        s = prevS[:]    # copy previous state space
+
+        for state in prevS:
+            sequence = state[0]
+            value = state[1]
+            
+            # Create new states and append to state space
+            newState1 = (sequence + "w", value**.5)
+            newState2 = (sequence + "f", math.floor(value))
+            
+            s.append(newState1)
+            s.append(newState2)
+
+            # Factorial of a not integer doesn't exist
+            try:
+                newState3 = (sequence + "!", math.factorial(value))
+            except  ValueError:
+                continue
+            except OverflowError:
+                continue
+
+            s.append(newState3)
+            
+        return s
